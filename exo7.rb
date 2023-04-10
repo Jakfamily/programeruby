e1310b9598696bd441a670c8b3a44c7cce0d2755
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-10 08:19:52 +0200

    ajout fichier 7

---
+++ exo7.rb
@@ -0,0 +1,20 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+#gets.chomp permet de demander a lu'ilisateur une information et renvoie la valeur stocker dans une variable
+
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+# puts mets un message dans la console 
+#print ajoute un symbole a gets.chomp
+# puis vas retourner la valeur stocker dans la variable user_name
+
+user_name = gets.chomp
+puts user_name
+
+# renvoi rien car absence de puts 
