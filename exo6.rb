39bc4599a1e29a6c2b364866e5e19b5037f42fa6
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-10 08:06:06 +0200

    ajout du fichier 6

---
+++ exo6.rb
@@ -0,0 +1,11 @@
+number_of_hours_worked_per_day = 10 #stock le nombre d'heure dans une variable
+number_of_days_worked_per_week = 5 # stock le nombre de jour par semaine
+number_of_weeks_in_THP = 11 # stock le nombre de week end a thp
+
+# imprime travaille puis dans #{} on multiplier les variable afin de pouvoir imprimer le resultat
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+# vas retourner une erreur car la variable minute n'est pas definie 
