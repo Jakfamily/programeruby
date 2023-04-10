6768b43467caeec8ffd5e9f12c4baecf237f1f57
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-10 09:11:19 +0200

    ajout du fichier 11

---
+++ exo11.rb
@@ -0,0 +1,6 @@
+puts "choisis un nombre"
+number = gets.chomp.to_i #convertie le nombre en entier 
+# boucles loops repte une action a mon programme
+number.times do
+    puts "salut ça farte ?"
+end
