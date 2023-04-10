632befb4ff0b9516215cf10ee473406678560818
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-10 08:00:00 +0200

    ajout du fichier 5

---
+++ exo5.rb
@@ -0,0 +1,23 @@
+puts "On va compter le nombre d'heures de travail à THP" # inprime la string
+puts "Travail : #{10 * 5 * 11}" #multiplie le nombre d'heure
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #multiplie le nombre de minute
+
+puts "Et en secondes ?" # imprime la string
+
+puts 10 * 5 * 11 * 60 * 60 # multiplie les seconde 
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #imprime la string
+
+puts 3 + 2 < 5 - 7 # comparaison en retournant true ou false
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # imprime la string puis pose l'operation
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # imprime la string puis pose l'operation
+
+puts "Ok, c'est faux alors !" # imprime la string
+
+puts "C'est drôle ça, faisons-en plus :" # imprime la string
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #imprime la string puis effectue la comparaison 
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #imprime la string puis effectue la comparaison 
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #imprime la string puis effectue la comparaison 
+ 
